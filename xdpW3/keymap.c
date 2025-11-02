53d1dc77f24547447badb4e794581e9ccac0c882
gui <guihm.dev@gmail.com>
2025-11-02 18:24:53 -0300

    fix tap dance

--- xdpW3/keymap.c
+++ xdpW3/keymap.c
@@ -68,9 +68,9 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_DELETE,                                      DUAL_FUNC_0,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TD(DANCE_2),
+    KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_DELETE,                                      DUAL_FUNC_0,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TD(DANCE_0),
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           DUAL_FUNC_1,
-    KC_ESCAPE,      MT(MOD_LCTL, KC_A),SFT_T(KC_S),MT(MOD_LALT, KC_D),MT(MOD_LGUI, KC_F),TD(DANCE_0),    TD(DANCE_1),                                                                    TD(DANCE_1),    TD(DANCE_4),    MT(MOD_RCTL, KC_J),MT(MOD_RSFT, KC_K),MT(MOD_LALT, KC_L),TD(DANCE_2),    TD(DANCE_3),
+    KC_ESCAPE,      MT(MOD_LCTL, KC_A),SFT_T(KC_S),MT(MOD_LALT, KC_D),MT(MOD_LGUI, KC_F),KC_G,    DUAL_FUNC_0,                                                                    DUAL_FUNC_1,    TD(DANCE_1),    MT(MOD_RCTL, KC_J),MT(MOD_RSFT, KC_K),MT(MOD_LALT, KC_L),TD(DANCE_2),    TD(DANCE_3),
     KC_SPACE,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         DUAL_FUNC_2,    DUAL_FUNC_3,
     TT(4),          KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_RIGHT,       QK_REPEAT_KEY,                                                                                                          QK_LEAD,        KC_UP,          KC_DOWN,        KC_TRANSPARENT, KC_TRANSPARENT, DUAL_FUNC_4,
     MO(1),          MO(2),          TG(3),                          KC_ENTER,       KC_BSPC,        MT(MOD_RGUI, KC_SPACE)
